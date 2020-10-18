e7feff0bd453e44ec18ccd14f2fcea59763d0000
Fuka <weendibol21300@gmail.com>
2020-10-18 12:32:22 +0600

    add ingnore upload folders and rules to emty file

--- source/module/save.excel.js
+++ source/module/save.excel.js
@@ -5,6 +5,8 @@ const { keyboard } = require('telegraf/markup')
 const dataConfig = require('../../data.config')
 
 exports.save = (ctx, data) => {
+    if(data.length === 0) return ctx.reply(`При сканировании электронные почты не были найдены`, keyboard(dataConfig.MAIN_MENU).oneTime().resize().extra())
+
     const path_file = `source/upload/${ctx.from.id}_${ctx.session.selected_name}.csv`
 
     try {
