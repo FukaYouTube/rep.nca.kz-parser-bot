885173541c3da39665574c50c5052e6f91b25d07
Fuka <weendibol21300@gmail.com>
2020-10-19 16:05:12 +0600

    update file

--- source/module/main.parser.js
+++ source/module/main.parser.js
@@ -4,13 +4,12 @@ const cheerio = require('cheerio')
 const { removeKeyboard } = require('telegraf/markup')
 
 const dataConfig = require('../../data.config')
-const { id } = require('../stage/custom.stage')
 const excel = require('./save.excel')
 
 exports.mainParser = async (ctx, start, end, url) => {
     ctx.replyWithMarkdown(`Идет сканирование сайта, *ожидайте*! \nВыбрана из главного меню: "${ctx.session.selected_name}"`, removeKeyboard().oneTime().resize().extra())
 
-    const { data } = await axios.get(url.url1 + start + url.url2)
+    const { data } = await axios.get(`${url.url1}1${url.url2}`)
     const $ = cheerio.load(data)
 
     const endPagesNumberInHTML = $('.text tr td').eq(-2).html()
@@ -31,19 +30,11 @@ exports.mainParser = async (ctx, start, end, url) => {
 
 exports.scanner = async (ctx, start, end, url) => {
     for(let i = start; i <= end; i++){
-        if(dataConfig.DATA_ITEMS.length >= 3250){
-            ctx.session.end_page = i - 1
-            ctx.replyWithMarkdown(`Память переполнена! Отправляю файл... \nЧтобы продолжить загрузку, выберите "Продолжить загрузку"`)
-            
-            dataConfig.DATA_ITEMS.splice(0, 1)
-            this.findEmail(dataConfig.DATA_ITEMS)
+        if(dataConfig.EMAIL_ITEMS.length >= 3000){
+            ctx.reply(`Память переполнен! Отправляю файл \nПродолжу загрузку через несколько минут...`)
+            excel.save(ctx, ctx.session.end_page)
 
-            ctx.reply(`Готово! Отправляю excel файл...`)
-            excel.save(ctx, dataConfig.EMAIL_ITEMS, i)
-
-            dataConfig.DATA_ITEMS = []
             dataConfig.EMAIL_ITEMS = []
-            return null
         }
 
         ctx.reply(`Выполняется поиск, ожидайте... \nПерехожу по страницам сайта: ${i} из ${end}`)
@@ -52,27 +43,15 @@ exports.scanner = async (ctx, start, end, url) => {
         let selector = cheerio.load(nextPage.data)
 
         selector('.text tr td a').each((index, element) => {
+            const emailRegex = /(?:[A-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g
             const values = selector(element).text()
-            dataConfig.DATA_ITEMS.push({ values })
-        })
 
-        dataConfig.DATA_ITEMS.splice(0, 1)
-        this.findEmail(dataConfig.DATA_ITEMS)
+            if(emailRegex.test(values)){
+                dataConfig.EMAIL_ITEMS.push(values.match(emailRegex)[0])
+            }
+        })   
     }
 
-    ctx.reply(`Готово! Отправляю excel файл...`)
-    excel.save(ctx, dataConfig.EMAIL_ITEMS)
-
-    dataConfig.DATA_ITEMS = []
-    dataConfig.EMAIL_ITEMS = []
-}
-
-exports.findEmail = data => {
-    let email = /(?:[A-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g
-
-    data.forEach((d, i) => {
-        if(email.test(d.values)){
-            dataConfig.EMAIL_ITEMS.push(d.values.match(email)[0])
-        }  
-    })
+    ctx.reply(`Готово, ожидайте файл...`)
+    excel.save(ctx)
 }
