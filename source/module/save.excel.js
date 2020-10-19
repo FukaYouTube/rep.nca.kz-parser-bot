c5d3c271e4a37f14b26ebdfb83eaaf8e7603f093
Fuka <weendibol21300@gmail.com>
2020-10-19 16:05:18 +0600

    update file

--- source/module/save.excel.js
+++ source/module/save.excel.js
@@ -7,8 +7,8 @@ const { keyboard } = require('telegraf/markup')
 
 const dataConfig = require('../../data.config')
 
-exports.save = async (ctx, data, end_page) => {
-    if(data.length === 0) return ctx.reply(`При сканировании электронные почты не были найдены`, keyboard(dataConfig.MAIN_MENU).oneTime().resize().extra())
+exports.save = async (ctx, end_page) => {
+    if(dataConfig.EMAIL_ITEMS.length === 0) return ctx.reply(`При сканировании электронные почты не были найдены`, keyboard(dataConfig.MAIN_MENU).oneTime().resize().extra())
 
     const path_file = `source/upload/${ctx.from.id}_${ctx.session.selected_name}_${end_page || 0}.xlsx`
     if(fs.existsSync(path_file)) fs.unlinkSync(path_file)
@@ -17,17 +17,20 @@ exports.save = async (ctx, data, end_page) => {
         const workheet = excel.addWorksheet('Email')
 
         workheet.columns = [{ header: 'Электронные почты', key: 'email', width: 50 }]
-        data.forEach(email => workheet.addRow({ email }))
+        dataConfig.EMAIL_ITEMS.forEach(email => workheet.addRow({ email }))
         workheet.getRow(1).eachCell(cell => cell.font = { bold: true })
     }catch(err){
         excel.removeWorksheet('Email')
         const workheet = excel.addWorksheet('Email')
 
         workheet.columns = [{ header: 'Электронные почты', key: 'email', width: 50 }]
-        data.forEach(email => workheet.addRow({ email }))
+        dataConfig.EMAIL_ITEMS.forEach(email => workheet.addRow({ email }))
         workheet.getRow(1).eachCell(cell => cell.font = { bold: true })
     }
 
     await excel.xlsx.writeFile(path_file)
+    
+    end_page ? 
+    ctx.replyWithDocument({ source: path_file }) :
     ctx.replyWithDocument({ source: path_file }, keyboard(dataConfig.MAIN_MENU).oneTime().resize().extra())
 }
